c719841a285d65cd8a6b2fa2d4b07de5fd7470f7
Chris <christophzeller@users.noreply.github.com>
2020-12-13 23:17:26 +0100

    Update walker.py

--- walker.py
+++ walker.py
@@ -1,8 +1,28 @@
-import collections.Counter
+import ast
+import collections
+import os
+
+def collect_data():
+    data_dicts = []
+    raw_files = os.listdir('dicts')
+    for raw_file in raw_files:
+        with open('dicts/' + raw_file) as fd:
+            raw_data = fd.read()
+            py_dict = ast.literal_eval(raw_data)
+            data_dicts.append(py_dict)
+    return data_dicts
+
+
+def accumulate_data(data_dicts):
+    c = collections.Counter()
+    for data_dict in data_dicts:
+        c.update(data_dict)
+    return c
+
 
 def main():
-    print('yolo')
-    return
+    print(accumulate_data(collect_data()))
+    
 
 if __name__ == '__main__':
     main()

410c1761ce5edc2ec5a2f71404a76d77eaf1165d
Chris <christophzeller@users.noreply.github.com>
2020-12-15 11:32:17 +0100

    Update walker.py

--- walker.py
+++ walker.py
@@ -1,5 +1,6 @@
 import ast
 import collections
+import matplotlib.pyplot as plt
 import os
 
 def collect_data():
@@ -20,8 +21,31 @@ def accumulate_data(data_dicts):
     return c
 
 
+def create_activity_graph(data_dicts):
+    fig = plt.figure()
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    ax.set_title('Fatzilla')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('# Activities')
+    
+    activity_dates = os.listdir('dicts')
+    
+    activity_counters = []
+    for data_dict in data_dicts:
+        activity_counters.append(len(data_dict))
+    
+    ax.bar(activity_dates, activity_counters, width=0.1)
+    
+    plt.xticks(range(0, max(activity_counters), 1))
+    plt.savefig('fatzilla.png')
+    plt.show()
+    
+
 def main():
-    print(accumulate_data(collect_data()))
+    data_dicts = collect_data()
+    
+    print(accumulate_data(data_dicts))
+    create_activity_graph(data_dicts)
     
 
 if __name__ == '__main__':
